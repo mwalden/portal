f2f92a4b52fbb4299d82e5e52f99751889f15011
mwalden <tont0r@hotmail.com>
2019-07-16 11:05:26 -0400

    phase 1

---
+++ Assets/HackWeek/Scripts/ARController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using GoogleARCore;
+
+
+#if UNITY_EDITOR
+
+#endif
+using input = GoogleARCore.InstantPreviewInput;
+public class ARController : MonoBehaviour
+{
+    private List<TrackedPlane> planes = new List<TrackedPlane>();
+    public GameObject grid;
+    public GameObject door;
+    public bool enabledDoor;
+    public GameObject arCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+
+
+        if (!Session.Status.Equals(SessionStatus.Tracking))
+        {
+            return;
+        }
+        Session.GetTrackables<TrackedPlane>(planes, TrackableQueryFilter.New);
+
+        foreach (TrackedPlane plane in planes)
+        {
+            GameObject go = Instantiate(grid, Vector3.zero, Quaternion.identity, transform);
+            go.GetComponent<GridVisualizer>().Initialize(plane);
+        }
+        Touch touch;
+        if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began){
+            return;
+        }
+        TrackableHit hit;
+
+        if (Frame.Raycast(touch.position.x,touch.position.y, TrackableHitFlags.PlaneWithinPolygon,out hit))
+        {
+            enabledDoor = true;
+            door.SetActive(true);
+
+            Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+            door.transform.position = hit.Pose.position;
+            door.transform.rotation = hit.Pose.rotation;
+
+            Vector3 cameraPosition = arCamera.transform.position;
+            //portal should rotate around y axis
+
+            cameraPosition.y = hit.Pose.position.y;
+
+            door.transform.LookAt(cameraPosition, door.transform.up);
+
+            door.transform.parent = anchor.transform;
+        }
+    }
+}
---
+++ Assets/HackWeek/Scripts/PortalManager.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Rendering;
+
+public class PortalManager : MonoBehaviour
+{
+
+    public GameObject mainCamera;
+    public GameObject sponza;
+    public Text text;
+    public Text distance;
+    public Material[] sponzaMaterials;
+    public bool insideBuilding;
+    public bool colliderExited;
+    public bool enabledCalled;
+    // Start is called before the first frame update
+    void Start()
+    {
+        sponzaMaterials = sponza.GetComponent<Renderer>().sharedMaterials;
+    }
+
+    private void Update()
+    {
+        Vector3 camPositionInPortalSpace = transform.InverseTransformPoint(mainCamera.transform.position);
+        distance.text = camPositionInPortalSpace.y.ToString() + " :: " + sponzaMaterials[0].GetInt("_StencilComp") + " :: " + enabledCalled;
+    }
+
+    private void OnEnable()
+    {
+        sponzaMaterials = sponza.GetComponent<Renderer>().sharedMaterials;
+        for (int x = 0; x < sponzaMaterials.Length; x++)
+        {
+            enabledCalled = true;
+            sponzaMaterials[x].SetInt("_StencilComp", 3);
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (colliderExited)
+        {
+            text.text = "Entered";
+        }
+    }
+
+    // Update is called once per frame
+    void OnTriggerStay(Collider col)
+    {
+        Vector3 camPositionInPortalSpace = transform.InverseTransformPoint(mainCamera.transform.position);
+        if (camPositionInPortalSpace.y < .5f )
+        {
+            text.text = camPositionInPortalSpace.y + " :: setting to Always" + sponzaMaterials[0].GetInt("_StencilComp");
+            //disable stencil test
+            for (int x = 0; x < sponzaMaterials.Length;x++)
+                sponzaMaterials[x].SetInt("_StencilComp", (int)CompareFunction.Always);
+
+        }
+        else
+        {
+            text.text = camPositionInPortalSpace.y + " :: setting to equal" + sponzaMaterials[0].GetInt("_StencilComp");
+            for (int x = 0; x < sponzaMaterials.Length; x++)
+            {
+                sponzaMaterials[x].SetInt("_StencilComp", (int)CompareFunction.Equal);
+            }
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        colliderExited = true;
+        text.text = "Exited";
+    }
+
+
+}
